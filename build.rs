01c034cc2adf61c45244325541950d2497ba276d
chevybeef <4460255+chevybeef@users.noreply.github.com>
2026-07-27 18:12:10 +0100

    fix dirty flag not showing

--- build.rs
+++ build.rs
@@ -2,12 +2,16 @@ use std::error::Error;
 use vergen_gitcl::{Emitter, GitclBuilder};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let gitcl = GitclBuilder::default()
-        .sha(true) // VERGEN_GIT_SHA (short by default in recent versions; see note)
-        .dirty(true) // VERGEN_GIT_DIRTY
-        .build()?;
+    let gitcl = GitclBuilder::default().sha(true).dirty(true).build()?;
 
-    Emitter::default().add_instructions(&gitcl)?.emit()?;
+    Emitter::default()
+        .fail_on_error()
+        .add_instructions(&gitcl)?
+        .emit()?;
+
+    // Fix: Force Cargo to re-evaluate build.rs when files change
+    println!("cargo:rerun-if-changed=Cargo.toml");
+    println!("cargo:rerun-if-changed=src");
 
     Ok(())
 }
